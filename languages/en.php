b282cd04737991a7626314d5277fd5874a7c06e1
Matt Beckett <beck24@gmail.com>
2013-01-17 10:41:11 -0700

    adds river views for set creation

--- languages/en.php
+++ languages/en.php
@@ -4,58 +4,61 @@ $english = array(
 	'au_sets:set' => 'Set',
 	'au_sets:sets' => 'Sets',
 	'item:object:au_set' => 'Sets',
-	'au_sets:enablesets' => "Enable group sets",
-	'au_sets:group' => 'Group sets',
-	'au_sets:title:user_sets' => '%s\'s sets',
-	'au_sets:title:all_sets' => "All site sets",
-	'au_sets:none' => "No sets",
-	'au_sets:title:friends' => 'Friends\' sets',
+	'au_sets:enablesets' => "Enable group Sets",
+	'au_sets:group' => 'Group Sets',
+	'au_sets:title:user_sets' => '%s\'s Sets',
+	'au_sets:title:all_sets' => "All site Sets",
+	'au_sets:none' => "No Sets",
+	'au_sets:title:friends' => 'Friends\' Sets',
 	'au_sets:message:deleted' => "Set has been deleted",
-	'au_sets:error:cannot_delete' => "Could not delete the set",
-	'au_sets:error:not_found' => "Could not find the set",
-	'au_sets:error:invalid:set' => "Invalid set",
+	'au_sets:error:cannot_delete' => "Could not delete the Set",
+	'au_sets:error:not_found' => "Could not find the Set",
+	'au_sets:error:invalid:set' => "Invalid Set",
 	'au_sets:error:invalid:entity' => "Invalid entity",
-	'au_sets:error:cannot:edit' => "You do not have permission to edit this set",
+	'au_sets:error:cannot:edit' => "You do not have permission to edit this Set",
 	'au_sets:error:invalid:user' => "Invalid user",
-	'au_sets:error:recursive:pin' => "Cannot pin a set to itself",
+	'au_sets:error:recursive:pin' => "Cannot pin a Set to itself",
 	'au_sets:pin' => "Pin",
 	'au_sets:pin:to:this' => "Pin it",
 	'au_sets:error:timeout' => "A timeout error has occurred...",
 	'au_sets:error:generic' => "An error has occurred...",
-	'au_sets:pin:to' => "Pin to a set",
-	'au_sets:search' => "Search for a set",
-	'au_sets:search:help' => "Type a word or words that appears in the title or description of the set you are looking for",
+	'au_sets:pin:to' => "Pin to a Set",
+	'au_sets:search' => "Search for a Set",
+	'au_sets:search:help' => "Type a word or words that appears in the title or description of the Set you are looking for",
 	'au_sets:success:pinned' => "Content has been pinned",
 	
 	// Editing
-	'au_sets:add' => 'Add set',
-	'sets:add' => 'Add set',
-	'au_sets:edit' => 'Edit set',
+	'au_sets:add' => 'Add Set',
+	'sets:add' => 'Add Set',
+	'au_sets:edit' => 'Edit Set',
 	'au_sets:body' => 'Body',
 	'au_sets:never' => 'Never',
 	'au_sets:label:icon' => "Set Icon (Leave blank to remain unchanged)",
-	'au_sets:label:write_access' => "Who can edit this set?",
+	'au_sets:label:write_access' => "Who can edit this Set?",
 	
 	// messages
-	'au_sets:error:cannot_save' => 'Cannot save set.',
-	'au_sets:error:cannot_edit' => 'This set may not exist or you may not have permissions to edit it.',
-	'au_sets:error:post_not_found' => "This set cannot be found",
+	'au_sets:error:cannot_save' => 'Cannot save Set.',
+	'au_sets:error:cannot_edit' => 'This Set may not exist or you may not have permissions to edit it.',
+	'au_sets:error:post_not_found' => "This Set cannot be found",
 	'au_sets:error:missing:title' => "Title cannot be empty",
 	'au_sets:error:cannot_write_to_container' => "Error - cannot write to container",
 	'au_sets:error:cannot_save' => "Error - cannot save the value %s",
 	'au_sets:message:saved' => "Set has been saved",
-	'au_sets:error:cannot_save' => "Cannot save set",
+	'au_sets:error:cannot_save' => "Cannot save Set",
+	
+	// river
+	'au_sets:river:create' => "%s has created a new Set %s",
 	
 	/* notifications */
-	'au_sets:newset' => 'A new set has been created',
+	'au_sets:newset' => 'A new Set has been created',
 	'au_sets:notification' =>
 '
-%s made a new blog post.
+%s made a new Set.
 
 %s
 %s
 
-View and comment on the new blog post:
+View and comment on the new Set:
 %s
 ',
 );
--- views/default/river/object/au_set/create.php
+++ views/default/river/object/au_set/create.php
@@ -1,10 +1,22 @@
 <?php
 
 $object = $vars['item']->getObjectEntity();
-$excerpt = strip_tags($object->excerpt);
-$excerpt = elgg_get_excerpt($excerpt);
+$subject = $vars['item']->getSubjectEntity();
+
+$excerpt = elgg_get_excerpt(strip_tags($object->description));
+
+$summary = elgg_echo('au_sets:river:create', array(
+	elgg_view('output/url', array('text' => $subject->name, 'href' => $subject->getURL())),
+	elgg_view('output/url', array('text' => $object->title, 'href' => $subject->getURL()))
+));
+
+$message = elgg_view_image_block(
+		elgg_view_entity_icon($object, 'small'),
+		$excerpt
+);
 
 echo elgg_view('river/elements/layout', array(
 	'item' => $vars['item'],
-	'message' => $excerpt,
+	'message' => $message,
+	'summary' => $summary
 ));
