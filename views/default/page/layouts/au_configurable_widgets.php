bff91b798c8e2245db7621d418c9c6623a58ea5f
RiverVanRain <river.vanrain@gmail.com>
2013-03-05 02:41:15 +0300

    Fix show the add widgets button and help text

--- views/default/page/layouts/au_configurable_widgets.php
+++ views/default/page/layouts/au_configurable_widgets.php
@@ -34,7 +34,7 @@ elgg_push_context('widgets');
 $widgets = elgg_get_widgets($owner->guid, $context);
 $preview = false;
 
-if (elgg_can_edit_widget_layout($context)) {
+if (elgg_instanceof($owner, 'object', 'au_set') && $owner->canEdit() || elgg_is_admin_logged_in()) {
 	if ($show_add_widgets) {
 		echo elgg_view('page/layouts/widgets/add_button');
 	}
