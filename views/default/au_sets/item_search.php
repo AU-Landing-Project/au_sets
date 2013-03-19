148120e3f77e3de87118e5e6c952b66860dba7b5
Matt Beckett <beck24@gmail.com>
2013-03-18 22:41:16 -0600

    enhancements to single pin widget and pinboard search

--- views/default/au_sets/item_search.php
+++ views/default/au_sets/item_search.php
@@ -23,24 +23,42 @@ $items = elgg_get_entities_from_relationship(array(
 	  'relationship_guid' => $vars['entity']->guid,
 	  'relationship' => AU_SETS_PINNED_RELATIONSHIP,
 	  'inverse_relationship' => true,
-	  'full_view' => false,
 	  'order_by' => 'r.time_created DESC',
-	  'pagination' => false
+	  'limit' => false,
   ));
 
 if ($items) {
   foreach ($items as $item) {
 	
 	$default_icon = elgg_get_site_url() . '_graphics/icons/default/tiny.png';
-	if ($item->getIconURL('tiny') == $default_icon) {
-	  $icon_subject = $item->getOwnerEntity();
+	$owner = $item->getOwnerEntity();
+	
+	if ($owner) {
+	  $owner_link = elgg_view('output/url', array('href' => $owner->getURL(), 'text' => $owner->title ? $owner->title : $owner->name));
 	}
 	
+	if ($item->getIconURL('tiny') == $default_icon) {
+	  $icon_subject = $owner;
+	}
+	else {
+	  $icon_subject = $item;
+	}
+	
+	$icon = elgg_view_entity_icon($icon_subject, 'tiny', array('use_hover' => false, 'href' => false, 'use_link' => false));
+	
+	$body = elgg_view('output/url', array(
+		'text' => $item->title ? $item->title : $item->name,
+		'href' => $item->getURL()
+	));
+	
+	$body .= elgg_view('output/longtext', array(
+		'value' => $owner_link . '&nbsp;' . elgg_view_friendly_time($item->time_created),
+		'class' => 'elgg-subtext'
+	));
+	
+	//echo $item->getIconURL('tiny');
 	echo '<div class="au-set-item-preview" data-widget="' . $vars['widget_guid'] . '" data-item="' . $item->guid . '">';
-	echo elgg_view_image_block(
-			elgg_view_entity_icon($icon_subject, 'tiny', array('use_hover' => false, 'href' => false, 'use_link' => false)),
-			$item->title ? $item->title : $item->name
-	);
+	echo elgg_view_image_block($icon, $body);
 	echo '</div>';
   }
 }
--- views/default/au_sets/search_results.php
+++ views/default/au_sets/search_results.php
@@ -48,7 +48,7 @@ $options['joins'][] = "JOIN {$dbprefix}objects_entity o ON e.guid = o.guid";
 $options['wheres'] = array();
 
 // only search others if 'mine' is not set
-if (!$mine) {
+if (!$mine && !elgg_is_admin_logged_in()) {
   //only show if we have write access to it
   $write_in = implode(', ', $write_accesses);
   $friends_in = "SELECT guid_one FROM {$dbprefix}entity_relationships WHERE guid_two = {$user->guid} AND relationship = 'friend'";
@@ -62,18 +62,10 @@ if (!$mine) {
   $options['joins'][] = "JOIN {$dbprefix}metadata md ON e.guid = md.entity_guid AND md.name_id = {$metadata_name_id}";
   $options['joins'][] = "JOIN {$dbprefix}metastrings ms ON ms.id = md.value_id";
 }
-else {
+elseif ($mine) {
   $options['owner_guids'] = array($user->guid);
 }
 	
-/*
- * @TODO - Jon wants everything shown, thinks it's better UX
- * leaving here in case he changes his mind
- * 
-// don't show anything already pinned
-$options['wheres'][] = "NOT EXISTS ( SELECT 1 FROM {$dbprefix}entity_relationships WHERE guid_two = e.guid AND guid_one = {$entity->guid} AND relationship = '" . AU_SETS_PINNED_RELATIONSHIP . "' )";
- * 
- */
 
 // prevent a set from pinning itself
 $options['wheres'][] = "e.guid != {$entity->guid}";
@@ -81,9 +73,11 @@ $options['wheres'][] = "e.guid != {$entity->guid}";
 
 // if we're doing a search
 if ($query) {
-  $options['selects'] = array("MATCH(o.title, o.description) AGAINST('$query') as relevance");
-  $options['wheres'][] = "MATCH(o.title, o.description) AGAINST('$query')";
-  $options['order_by'] = 'relevance DESC';
+  //$options['selects'] = array("MATCH(o.title, o.description) AGAINST('$query') as relevance");
+  //$options['wheres'][] = "MATCH(o.title, o.description) AGAINST('$query')";
+  //$options['order_by'] = 'relevance DESC';
+  $options['wheres'][] = "(o.title LIKE '%{$query}%' OR o.description LIKE '%{$query}%')";
 }
 
+//echo '<pre>' . print_r($options,1) . '</pre>';
 echo elgg_list_entities_from_metadata($options);
