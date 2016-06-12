9a07edb1b42c9feaa8f424a4d05be77408941d7b
ura soul <ura@ureka.org>
2016-06-12 15:18:50 +0100

    Revert "fixed: wrong widgets displayed in widget picker."
    This reverts commit 2e8d1ff2940fbd47e81618058752a5d7fec1f378.

--- views/default/page/layouts/au_configurable_widgets.php
+++ views/default/page/layouts/au_configurable_widgets.php
@@ -8,11 +8,11 @@
  * @uses $vars['show_add_widgets'] Display the add widgets button and panel (true)
  * @uses $vars['exact_match']      Widgets must match the current context (false)
  * @uses $vars['show_access']      Show the access control (true)
- *
- *
+ * 
+ * 
  * widget_layout example
  *	array(array('33', '34', '33')) = 1 row with columns 33%, 34%, 33%
- *
+ * 
  *  array(array('100'), array('50', '50'), array('100')) = 3 rows, 100%, 50/50, 100%
  */
 
@@ -24,12 +24,13 @@ $show_access = elgg_extract('show_access', $vars, true);
 $owner = elgg_get_page_owner_entity();
 
 $context = elgg_get_context();
+
 $available_widgets_context = elgg_trigger_plugin_hook("available_widgets_context", "widget_manager", array(), $context);
 
-// ADDED TRUE to this call, otherwise we get all widgets returned
-$widget_types = elgg_get_widget_types($available_widgets_context,true);
+$widget_types = elgg_get_widget_types($available_widgets_context);
 
 elgg_push_context('widgets');
+
 $widgets = elgg_get_widgets($owner->guid, $context);
 
 // are we in view or layout mode?
@@ -37,7 +38,7 @@ $min_height = '0px';
 $preview = false;
 if ($owner->canEdit()) {
   $preview = get_input('view_layout', false);
-
+  
 	if ($preview) {
 	  $helptext = elgg_echo('au_sets:mode:layout:help', array(
 		  elgg_view('output/url', array(
@@ -62,13 +63,9 @@ echo "<div class='elgg-layout-widgets layout-widgets-" . $context . "'>";
 
 if (elgg_can_edit_widget_layout($context)) {
 	if ($show_add_widgets) {
-		// added params to get the correct content of the panel
-		echo elgg_view('page/layouts/widgets/add_button', array('context' => $context,
-		'widgets' => $widgets,
-		'exact_match' => $exact_match,
-		'show_access' => $show_access));
+		echo elgg_view('page/layouts/widgets/add_button');
 	}
-
+	
 	$params = array(
 		'widgets' => $widgets,
 		'context' => $context,
@@ -77,6 +74,7 @@ if (elgg_can_edit_widget_layout($context)) {
 	);
 	echo elgg_view('page/layouts/widgets/add_panel', $params);
 }
+
 
 if ($owner->canEdit()) {
   echo elgg_view('output/longtext', array('value' => $helptext, 'class' => 'elgg-subtext au-sets-pinboard-help'));
@@ -88,33 +86,33 @@ $column_index = 0;
 foreach ($widget_columns as $row => $columns) {
   foreach ($columns as $width) {
 	$column_index++;
-
+	
 	if (isset($widgets[$column_index])) {
 	  $column_widgets = $widgets[$column_index];
 	}
 	else {
 	  $column_widgets = array();
 	}
-
+	
 	$class = 'au-sets-widget-width-' . $width;
 	if (elgg_can_edit_widget_layout($context) && $preview) {
 	  $class .= ' au-sets-widget-editable';
 	}
 
 	echo "<div class=\"elgg-widgets au-sets-widgets au-sets-row au-sets-row-{$row} {$class}\" id=\"elgg-widget-col-$column_index\">";
-
+	
 	if ($preview) {
-
+	  
 	  // column header
 	  echo '<div class="au-sets-widget-view au-sets-widget-width-100 elgg-state-fixed" style="float: none;">';
 	  echo elgg_echo('au_sets:widget:column', array($column_index));
 	  echo '<div class="elgg-subtext">' . elgg_echo('au_sets:drag:widgets') . '</div>';
 	  echo '</div>';
-
+	  
 	  // background div
-
+	  
 	}
-
+	
 	if (sizeof($column_widgets) > 0) {
 	  foreach ($column_widgets as $widget) {
 		if (array_key_exists($widget->handler, $widget_types)) {
@@ -134,7 +132,7 @@ foreach ($widget_columns as $row => $columns) {
 	}
 	echo '</div>';
   }
-
+  
   // row complete, clear float
   echo '<div style="clear: both;"></div>';
 }
